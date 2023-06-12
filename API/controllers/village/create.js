9618825738599a5c2206719b5273eac2bc6daac4
Gabriel Inniss <gabevaughninn@gmail.com>
2023-06-12 19:40:16 -0400

    organizations modal form implemented

--- API/controllers/village/create.js
+++ API/controllers/village/create.js
@@ -2,11 +2,11 @@ const createVillage = async (req, res) => {
     const {
       session,
       db: { Village },
-      body: { name,image,location,user_id },
+      body: { name, image, location, user_id },
     } = req;
   
     // TODO: check if username is taken, what should you return?
-    const village = await Village.create(name, image, location,user_id);
+    const village = await Village.create(name, image, location, user_id);
     if(!village) return res.status(409).send('There is something wrong with the form you submitted.')
     res.send(village);
   };
