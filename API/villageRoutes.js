22f83983f7aa83e139dd5ec5b4eeec84479baba5
ginniss2022 <gabevaughninn@gmail.com>
2023-06-12 12:39:26 -0400

    Organization preparation rebase

--- API/villageRoutes.js
+++ API/villageRoutes.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const addModels = require('./middleware/add-models');
-const villageController = require('./controllers/village')
+const villageController = require('./controllers/village');
+
 const villageRouter = express.Router();
 villageRouter.use(addModels);
 
 villageRouter.get('/villages', villageController.list);
 villageRouter.get('/villages/:id', villageController.find);
 
-
 villageRouter.post('/villages', villageController.create);
 villageRouter.post('/villages/:id', villageController.join);
-
-
-
 
 module.exports = villageRouter;
