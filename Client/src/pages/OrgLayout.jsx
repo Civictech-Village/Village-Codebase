9d645223b19cb1627c76b7a0ec5136a19f5667d5
VTmeta <vlamanno2004@gmail.com>
2023-06-14 18:16:56 -0400

    worked on styling and page format

--- Client/src/pages/OrgLayout.jsx
+++ Client/src/pages/OrgLayout.jsx
@@ -1,14 +1,26 @@
 import { fetchHandler } from "../utils";
+import React, { useRef } from "react";
+import OrganizationPosts from "../components/OrganizationPosts";
 import Button from "@mui/material/Button";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import PostCards from "../components/PostCards";
+import MemberCard from "../components/MemberCard";
+import BackgroundVillage from "../components/BackgroundVillages";
+import Example from "../components/Modal";
+import BasicExample from "../components/NavTabs";
 export default function OrgLayoutPage() {
-    console.log('hello')
-    const [village, setVillage] = useState({ name: "", image: "" ,location:""});
+  console.log("hello");
 
+  //Village
+  const [village, setVillage] = useState({ name: "", image: "", location: "" });
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  
   useEffect(() => {
-    
     const doFetch = async () => {
-      const responseData = await fetchHandler('/villages/:id');
+      const responseData = await fetchHandler("/villages/:id");
       if (responseData) {
         const { name, image, location } = responseData;
         setVillage({ name, image, location });
@@ -16,41 +28,83 @@ export default function OrgLayoutPage() {
     };
 
     doFetch();
-  },[]); 
+  }, []);
+
+  //Posts
+  const [posts, setPosts] = useState({ username: "", message: "", image: "" });
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const responseData = await fetchHandler("/posts");
+      if (responseData) {
+        const { username, message, image } = responseData;
+        setPosts({ username, message, image });
+      }
+    };
+
+    doFetch();
+  }, []);
+  const props = {
+    profileUsername: "Williamsburg",
+    currentUser: { id: 1 },
+  };
+  //Members
+  const [members, setMembers] = useState({ username: "", profile_picture: "" });
+
+  useEffect(() => {
+    const doFetch = async () => {
+      const responseData = await fetchHandler("/users");
+      if (responseData) {
+        const { username, profile_picture } = responseData;
+        setMembers({ username, profile_picture });
+      }
+    };
+
+    doFetch();
+  }, []);
 
   return (
     <>
-      <div className="village">
-        <h1>{village.name}</h1>
-        <img src="{village.img}" alt="" />
-        <h3>{village.location}</h3>
+      <div>
+        <div className="village">
+          <h1>{village.name}</h1>
+          <img src="{village.img}" alt="" />
+          <h3>{village.location}</h3>
+        </div>
       </div>
-    <>
-    <div>
-      <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Follow
-              </Button>
-    </div>
-    <div>
-        <h1>Posts</h1>
-        <>
-        <h2>Profile:</h2>
-        <img src="" alt="" />
-        <p>Issue description</p>
-        <Button
-                type="submit"
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Like
-              </Button>
-        </>
-    </div>
-    </>
+      <BackgroundVillage props={props} />
+      <Example handleClose={handleClose} show={show}/>
+      <div style={{ display: "flex", maxHeight:"600px" }}>
+        <div
+          className="posts"
+          style={{borderRadius:"1px", display: "flex", backgroundColor:"grey",flexDirection: "column", width:"100%", justifyContent:"center", alignItems:"center" }}
+        >
+            <BasicExample></BasicExample>
+          <h1>Posts</h1>
+          <Button onClick={handleShow}>Create Post</Button>
+          <div style={{overflowY:'scroll', maxHeight:"55%"}}>
+          <PostCards />
+          <PostCards />
+          <PostCards />
+          <PostCards />
+
+          </div>
+
+          <h3>{posts.username}</h3>
+          <h3>{posts.message}</h3>
+        </div>
+
+        <div className="members" style={{textAlign:'center'}}>
+          <h1>Members</h1>
+          {/* <h2>{members.username}</h2>
+        <img src="{members.profile_picture}" alt="" /> */}
+          <MemberCard />
+          <MemberCard />
+          <Button type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Show More
+          </Button>
+        </div>
+      </div>
     </>
   );
-  }
+}
