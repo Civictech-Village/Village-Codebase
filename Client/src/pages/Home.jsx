921573a59cb923a7a2d999ed1ec5b24ae6278beb
InstantIsTheName <vantoinefils21@gmail.com>
2023-06-14 22:35:48 -0400

    Demo Homepage

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -2,29 +2,46 @@ import React from "react";
 
 function HomePage() {
   return (
-    <>
+    <div style={{ backgroundColor: '#b2f1d5', padding: '20px' }}>
       <h1>
         Village
       </h1>
       <p>
-        Welcome to Village
+        We aim to serve local community members who either just wish to stay in the know about what their community is experiencing or those who want to let their fellow community members know what issues they are experiencing.
       </p>
-    </>
+    </div>
   );
 }
-
-const paragraphs = (
-  <div id="i-am-the-outermost-element">
-    <p>I am a paragraph.</p>
-    <p>I, too, am a paragraph.</p>
-  </div>
-);
-
-export default function App() {
+function NYC() {
   return (
     <>
-      <paragraphs/>
+    <img src= "" alt="/home/vinny/Development/Village-Codebase/Client/src/pages/new-york-city-evening-NYCTG0221-52492d6ccab44f328a1c89f41ac02aea.jpg" />
+    </>
+  )
+}
+
+
+function Bio() {
+  return (
+    <div style={{ textAlign: 'center', padding: '20px' }}>
+      <p>Come together as a community, develop a Village</p>
+      <button style={{ marginTop: '10px', backgroundColor: '#0d6efd', padding: '10px 30px', borderRadius: '25px', color: 'white', border: 'none' }}>
+        Start a Village
+      </button>
+    </div>
+  );
+}
+
+
+
+function App() {
+  return (
+    <>
       <HomePage />
+      <NYC />
+      <Bio />
     </>
   );
 }
+
+export default App;
