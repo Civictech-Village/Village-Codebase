2e1ba84f8cb243181e3d46155f71bfd42d17ec41
VTmeta <114096133+VTmeta@users.noreply.github.com>
2023-06-27 10:43:46 -0400

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-import { useContext, useEffect } from 'react';
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUpPage from './pages/SignUp';
-import LoginPage from './pages/Login';
-import SiteHeadingAndNav from './components/SiteHeadingAndNav';
-import NotFoundPage from './pages/NotFound';
-import UserContext from './contexts/current-user-context';
-import { checkForLoggedInUser } from './adapters/auth-adapter';
-import UsersPage from './pages/Users';
-import UserPage from './pages/User';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import Organizations from './pages/Organizations'
-import OrgLayoutPage from './pages/OrgLayout'
-import LandingPage from './pages/LandingPage'
-import OrganizationPosts from './components/OrganizationPosts';
-=======
+
 import { useContext, useEffect } from "react";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
@@ -45,7 +26,6 @@ import Feed from "./pages/Feed";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
 import Messages from "./pages/Messages";
->>>>>>> main
 export default function App() {
   const { currentUser, setCurrentUser, setLoggedIn } =
     useContext(CurrentUserContext);
@@ -54,27 +34,6 @@ export default function App() {
     setLoggedIn(true)
   }, [setCurrentUser]);
 
-<<<<<<< HEAD
-  return <>
-    <SiteHeadingAndNav />
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <main>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path ='/LandingPage' element={<LandingPage/>} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/organizations' element={<Organizations />} />
-        <Route path='/organizations/:id' element={<OrgLayoutPage/>} />
-        <Route path='./components/OrganizationPosts' element={<OrganizationPosts/>} />
-        <Route path='/sign-up' element={<SignUpPage />} />
-        <Route path='/users' element={<UsersPage />} />
-        <Route path='/users/:id' element={<UserPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
-    </main>
-    </LocalizationProvider>
-  </>;
-=======
 
   return (
     <>
