1909f8ba9b9de7c9a2ff8f2e364ea56eda361d02
Knox-Phillips <114096538+Knox-Phillips@users.noreply.github.com>
2023-06-27 20:49:29 -0400

    Update Home.jsx

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,86 +1,18 @@
-import * as React from 'react';
-import { useContext, useState, useEffect } from 'react';
-import Home from '../assets/Home.jpg'
-import { Link } from "react-router-dom";
+import { motion, useTransform, useScroll } from "framer-motion";
+import SearchBar from "../components/NavBar";
+import Avatar from "../components/Avatar";
+import Tabs from "../components/Tabs";
+import HomeCard from "../components/HomeCard";
+import { useContext, useEffect, useState } from "react";
 import CurrentUserContext from "../contexts/current-user-context";
 import { Navigate, useNavigate } from "react-router-dom";
 import { fetchHandler } from "../utils";
 import Footer from "../components/LandingPage/Footer";
 import InfiniteScroll from "react-infinite-scroll-component";
 import CommentModal from "../components/CommentModal/CommentModal";
-import background from "/home/vinny/Development/Village-Codebase/Client/src/assets/tribeca-best-neighborhoods-manhattan.jpg";
-import OrganizationCard from '../components/OrganizationCard';
-import { createVillage, getAllVillages } from '../adapters/organizations-adapter';
-
 
 export default function HomePage() {
-const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-const [Organizations, setOrganizations] = React.useState([]);
-const [open, setOpen] = React.useState(true);
-useEffect(() => {
-  const fetchOrganizations = async() => {
-   const result =  await getAllVillages()
-   setOrganizations(result)
-  } 
-  fetchOrganizations()
-}, [open, setOpen])
-console.log(Organizations.splice(3))
-
-  return (
-    <div style={{ display: 'flex', padding: '2% 2%' }}>
-      <div style={{ flex: '1', marginRight: '10px' }}>
-        <h1 style={{ textAlign: 'left', color: 'Black'}}>Welcome {currentUser && currentUser.username}</h1>
-        <h2 style={{ marginTop: '10%', color: 'Black'}}>Recently Viewed:</h2>
-        {Organizations.map((organization, index) => <OrganizationCard key={index} village={organization}/>)}
-        <Bio />
-        <RecVillages />
-      </div>
-    </div>
-  );
-}
-
-function RecVillages() {
-  return (
-    <>
-    <h1>Recently Viewed Villages</h1>
-    </>
-  )
-}
-
-function Bio() {
-  return (
-    <div style={{ display: 'flex', justifyContent: 'flex-end', position: 'relative' }}>
-      <div style={{ backgroundColor: '#f7f7f8', borderRadius: '15%', marginLeft: '60%', marginRight: '10%', padding: '50px', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.5)', marginTop: '-50px', paddingBottom: '5%'}}>
-        <p style={{ marginBottom: '50%' }}>
-          We aim to serve local community members who either just wish to stay in the know about what their community is experiencing or those who want to let their fellow community members know what issues they are experiencing.
-        </p>
-        <p>Come together as a community, develop a Village</p>
-        <Link to={'/organizations'}>
-          <button style={{ marginTop: '10px', backgroundColor: '#b2f1d5', marginLeft: '30%', padding: '10px 30px', borderRadius: '25px', color: 'white', border: 'none' }}>
-            Start a Village
-          </button>
-        </Link>
-      </div>
-    </div>
-  );
-}
-
-
-
-// function NYC() {
-//   return ( 
-//     <div style={}>
-//       <img src="" alt="" />
-//     </div>
-//   )
-// }
-
-
-
-
-
-
-function App() {
+  const { scrollYProgress } = useScroll();
   const navigate = useNavigate();
   const [posts, setPosts] = useState([]);
   const [page, setPage] = useState(0);
@@ -160,9 +92,69 @@ function App() {
   };
 
   return (
-    <div style={{backgroundColor:'#b2f1d5', backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat: "no-repeat", objectFit: 'fill', backdropFilter: 'blur(10px)', height:"", display:'flex', alignItems:'center', flexDirection:'column',justifyContent:'center'}}>
-      <HomePage />
-      {/* <Bio /> */}
+    <div style={{ width: "100%", height: "100%" }}>
+      <CommentModal
+        isOpen={show}
+        closeModal={handleClose}
+        post={selectedPost}
+      />
+
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          height: "fit-content",
+          alignItems: "center",
+          padding: "10px",
+        }}
+      >
+        <SearchBar />
+        <Avatar />
+      </div>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: "6em",
+          minHeight: "100vh",
+          height: "100%",
+          marginBottom: "3em",
+        }}
+        id="posts"
+      >
+        <Tabs
+          handlePopular={handlePopular}
+          handleMyVillage={handleMyVillage}
+          activeTab={activeTab}
+        />
+
+        {posts.length > 0 ? (
+          <InfiniteScroll
+            dataLength={posts.length}
+            next={fetchMoreData}
+            hasMore={hasMore}
+            loader={<h4>Loading...</h4>}
+            loading={loading}
+          >
+            {posts.map((elem, i) => {
+              return (
+                <HomeCard
+                  key={i}
+                  props={elem}
+                  openModal={() => handleShow(elem)}
+                />
+              );
+            })}
+          </InfiniteScroll>
+        ) : (
+          <p style={{ margin: "auto" }}>
+            Sorry, There are no posts here for now
+          </p>
+        )}
+      </div>
+      <Footer />
     </div>
   );
 }
