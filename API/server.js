4cc6f89ea1d9507d13f9dec6f7787fca4887d7bb
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-06-28 10:43:03 -0400

    Pushing to main last time hopefully for socket fix

--- API/server.js
+++ API/server.js
@@ -21,44 +21,7 @@ cloudinary.config({
 });
 
 const app = express();
-const io = new Server({
-  cors: {
-    origin: 'https://village-codebase.onrender.com',
-  },
-  methods: ["GET", "POST"],
-  credentials:true
-});
-io.on('connection', (socket) => {
-  console.log('A user has connected');
 
-  socket.on('disconnect', () => {
-    console.log('A user has disconnected');
-  });
-
-  socket.on('send_message', (data) => {
-    console.log(data)
-    socket.emit("received", data)
-  })
-
-  socket.on("joinRoom", (room) => {
-    socket.join(room);
-  });
-
-  // Handle other socket events and logic here
-
-  socket.on("chat message", async (msg, id) => {
-    console.log(`Received message: ${msg}`, id);
-    const roomId = msg;
-    console.log(msg)
-    // io.emit("chat message", msg); // send the message only to clients in the same chat room
-    io.to(id).emit("chat message", msg)
-    // io.emit("chat message", msg)
-
-    if(msg.userId){
-      const result = await sendMessage(msg.message, msg.userId, id, msg.profilePic, msg.username);
-    }
-  });
-});
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
--- Client/src/socket.js
+++ Client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://village-codebase.onrender.com");
-//http://localhost:3000
+const socket = io("http://localhost:8080");
+//https://village-codebase.onrender.com/
 export default socket
