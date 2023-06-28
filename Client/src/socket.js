333e6f887d3702a898a97ead0582965450aac467
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-06-28 11:21:23 -0400

    Pushing up to main

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io("http://localhost:10000");
+const socket = io("https://village-codebase.onrender.com/:10000");
 //https://village-codebase.onrender.com/
 export default socket
