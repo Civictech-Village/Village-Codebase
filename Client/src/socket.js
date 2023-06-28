bfe3d4a622ce74bebc09cc262915907b0027f71e
Knox-Phillips <knox-phillips@citypolyhs.org>
2023-06-28 02:05:59 -0400

    Pushing port of deployed

--- Client/src/socket.js
+++ Client/src/socket.js
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
 
-const socket = io("https://village-codebase.onrender.com:3000");
-//http://localhost:3000
+const socket = io("http://localhost:3000");
 export default socket
